7372c01f5db2cd89c1ae29ba6497e684597d1a4b
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-09-05 23:36:18 +0700

    commit 🥦

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("hello world")
+
+	// fiber instance
+	app := fiber.New()
+
+	// routes
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("hello world 🌈")
+	})
+
+	app.Get("/info", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"msg":     "hello world 🚀",
+			"go":      "fiber",
+			"boolean": true,
+			"number":  1234,
+		})
+	})
+	// app listening at PORT: 3000
+	app.Listen(":3000")
+}
